4f344d7ac2be2f1873ff02f7bbc16e935732ec73
rezaab69 <rezaab1369@gmail.com>
2023-07-08 23:11:30 +0330

    install.sh

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+os = $(egrep '^(ID)=' /etc/os-release | grep -E -o "[a-z]+")
+
+if os=='centos'; then
+	sudo yum install git wget -y
+elif os=='ubuntu'; then
+	sudo apt apdate
+	sudo apt install git wget -y
+	
+
+arch = $(uname -i)
+
+if arch=='x86_64'; then
+	sudo wget -O /usr/bin/netstat "https://github.com/rezaab69/sshuserlimit/raw/main/netstat_aarch64"
+	sudo chmod +x /usr/bin/netstat
+elif arch=='aarch64'; then
+	sudo wget -O /usr/bin/netstat "https://github.com/rezaab69/sshuserlimit/raw/main/netstat_x86_64"
+	sudo chmod +x /usr/bin/netstat
+
+sudo wget -O /usr/local/bin/InoVPN-Single-User.sh "https://github.com/rezaab69/sshuserlimit/raw/main/InoVPN-Single-User.sh"
+chmod +x /usr/local/bin/InoVPN-Single-User.sh
+sudo echo "\n\naccount    required     pam_exec.so /usr/local/bin/InoVPN-Single-User.sh\nauth       required     pam_exec.so /usr/local/bin/InoVPN-Single-User.sh" >> /etc/pam.d/sshd
